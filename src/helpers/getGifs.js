adcc5cd22f476a67bbc759d94c99192d16e110a2
SiIverHawk <miguelnegro100@gmail.com>
2021-10-27 13:17:42 -0600

    Changed api

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=0xTRluEoUrM7EIfwEEFd4qvUm7V7gOju`
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=dBm29Rf9V7NbopvJHMNOw0PosLYDZh0q`
     const response = await fetch(url)
     const { data } = await response.json()
 
